ee50f8dbd419f7ce996249d56eddc1372cc24d12
otaka2024 <o.taka2010@gmail.com>
2026-05-25 23:59:34 +0900

    Initial release v1.0.0 - JR可部線通勤時刻表PWA

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'kabe-line-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './timetable.json',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).catch(() => {
+        if (e.request.destination === 'document') {
+          return caches.match('./index.html');
+        }
+      });
+    })
+  );
+});
